f0f982e64d9a8e0d62d80281f27c3262f06916c4
freedy8910 <65531734+freedy8910@users.noreply.github.com>
2020-05-25 16:02:57 +0800

    開頭畫面

---
+++ 2DRun/Assets/KID/Scripts/SceneController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace KID
+{
+    public class SceneController : MonoBehaviour
+    {
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity_TNU_2DRun
+ Unity 東南科大 2D 跑酷遊戲
