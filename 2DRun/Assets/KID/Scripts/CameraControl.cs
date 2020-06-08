b9a8dddd4a2e888196bdc4e2abde0a3891ae78aa
freedy8910 <65531734+freedy8910@users.noreply.github.com>
2020-06-08 15:55:03 +0800

    完成角色控制

---
+++ 2DRun/Assets/KID/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [Header("要追蹤的物件")]
+    public Transform target;
+    [Header("追蹤速度"), Range(1, 100)]
+    public float speed = 10;
+    [Header("追蹤上限與下限：X 為下方限制，Y 為上方限制")]
+    public Vector2 limit = new Vector2(0, 5);
+
+    private void LateUpdate()
+    {
+        Track();
+    }
+
+    private void Track()
+    {
+        Vector3 pos = target.position;
+        pos.z = -10;
+        pos.y = Mathf.Clamp(pos.y, limit.x, limit.y);
+
+        transform.position = Vector3.Lerp(transform.position, pos, 0.5f * Time.deltaTime * speed);
+    }
+}
---
+++ 2DRun/Assets/KID/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("移動速度"), Range(1, 1000)]
+    public float speed = 50;
+    [Header("跳躍高度"), Range(10, 1000)]
+    public float jump = 100;
+    [Header("動畫參數：跳躍")]
+    public string paraJump = "跳躍開關";
+    [Header("動畫參數：死亡")]
+    public string paraDead = "死亡觸發";
+    [Header("地板標籤")]
+    public string tagGround = "地板";
+    [Header("中心點位移")]
+    public Vector3 offset;
+    [Header("地板偵測長度")]
+    public float length = 0.1f;
+
+    private Rigidbody2D rig;
+    private Animator ani;
+    public bool isGrounded
+    {
+        get
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position + offset, transform.up, length);
+            return hit.collider.tag == tagGround;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position + offset, transform.up * length);
+    }
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Jump();
+        Move();
+    }
+
+    private void Move()
+    {
+        ani.SetBool(paraJump, !isGrounded);
+        transform.Translate(transform.right * speed * Time.deltaTime / 50);
+    }
+
+    private void Jump()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.AddForce(transform.up * jump);
+            rig.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+    }
+}
